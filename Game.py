766a56f571b245856b2603249052141084f4d951
Tang Yan <tangyan1412@foxmail.com>
2023-11-13 08:32:45 +0800

    init

---
+++ Game.py
@@ -0,0 +1,151 @@
+import numpy as np
+
+# 定义贪吃蛇的动作，
+# R表示向右边运动，就在在positon坐标的X轴加1,Y轴坐标不变，可以实现贪吃蛇头部坐标更新。
+player_moves = {
+    'L': np.array([-1., 0.]),
+    'R': np.array([1., 0.]),
+    'U': np.array([0., -1.]),
+    'D': np.array([0., 1.])
+}
+initial_playersize = 4
+
+
+class snakeclass(object):
+    def __init__(self, gridsize):
+        self.pos = np.array([gridsize // 2, gridsize // 2]).astype('float')  # 初始化贪吃蛇头部position坐标，位于显示区域的中心
+        self.dir = np.array([1., 0.])  # 初始化贪吃蛇的运动
+        self.len = initial_playersize  # 初始化贪吃蛇的长度
+        # 建立一个position数据用于存贮贪吃蛇历史轨迹，这些轨迹也是贪吃蛇的身子
+        self.prevpos = [np.array([gridsize // 2, gridsize // 2]).astype('float')]
+        self.gridsize = gridsize
+
+    def move(self):
+        self.pos += self.dir
+        self.prevpos.append(self.pos.copy())
+        self.prevpos = self.prevpos[-self.len - 1:]
+
+    def checkdead(self, pos):  # 判断贪吃蛇的头部是否到达边界或则碰到自己的身体。
+        if pos[0] <= -1 or pos[0] >= self.gridsize:
+            return True
+        elif pos[1] <= -1 or pos[1] >= self.gridsize:
+            return True
+        # 判断贪吃蛇头是否碰到了身体
+        elif list(pos) in [list(item) for item in self.prevpos[:-1]]:
+            return True
+        else:
+            return False
+
+    def inBody(self, pos):
+        return list(pos) in [list(item) for item in self.prevpos[:-1]]
+
+    def getproximity(self):  # 该函数定义了贪吃蛇头部的位置更新
+        L = self.pos - np.array([1, 0])
+        R = self.pos + np.array([1, 0])
+        U = self.pos - np.array([0, 1])
+        D = self.pos + np.array([0, 1])
+        possdirections = [L, R, U, D]
+        proximity = [int(self.checkdead(x)) for x in possdirections]
+        return proximity
+
+    def relativeForApple(self, applePos):
+        x = 0
+        y = 0
+        if self.pos[0] > applePos[0]:
+            x = 1
+        if self.pos[0] < applePos[0]:
+            x = -1
+        if self.pos[1] > applePos[1]:
+            y = 1
+        if self.pos[1] < applePos[1]:
+            y = -1
+        return np.array([x, y])
+
+    def __len__(self):
+        return self.len + 1
+
+
+class appleclass(object):  # 定义苹果出现的地方
+    def __init__(self, gridsize):
+        self.pos = np.random.randint(1, gridsize, 2)
+        self.score = 0
+        self.gridsize = gridsize
+
+    def eaten(self):
+        ## generate new apple every time the previous one is eaten
+        self.pos = np.random.randint(1, self.gridsize, 2)
+        self.score += 1
+
+
+class GameEnvironment(object):
+    def __init__(self, gridsize, nothing, dead, apple):
+        self.snake = snakeclass(gridsize)
+        self.apple = appleclass(gridsize)
+        self.game_over = False
+        self.gridsize = gridsize
+        self.reward_nothing = nothing
+        self.reward_dead = dead
+        self.reward_apple = apple
+        self.time_since_apple = 0
+
+    def resetgame(self):
+        self.apple.pos = np.random.randint(1, self.gridsize, 2).astype('float')  # 随机初始化苹果的位置
+        self.apple.score = 0  # 初始化score
+        self.snake.pos = np.random.randint(1, self.gridsize, 2).astype('float')  # 初始化贪吃蛇出现的位置
+        self.snake.prevpos = [self.snake.pos.copy().astype('float')]
+        self.snake.len = initial_playersize
+        self.game_over = False
+
+    def getFillCount(self):
+        self.snakeFlag = [[0] * self.gridsize for _ in range(self.gridsize)]
+
+        for pos in self.snake.prevpos:
+            if 0 <= pos[0] < self.gridsize and 0 <= pos[1] < self.gridsize:
+                self.snakeFlag[int(pos[0])][int(pos[1])] = 1
+
+        list = []
+        for x in range(self.gridsize):
+            for y in range(self.gridsize):
+                list.append(self.snakeFlag[x][y])
+        return list
+
+    def get_boardstate(self):
+        return [self.snake.pos, self.snake.dir, self.snake.prevpos, self.apple.pos, self.apple.score, self.game_over]
+
+    def update_boardstate(self, move):
+        reward = self.reward_nothing
+        Done = False
+        # python 中的all函数用于判断可迭代iterable中所有元素是否都是True，如果是返回Treu，否则False
+        if move == 0:
+            if not (self.snake.dir == player_moves['R']).all():
+                self.snake.dir = player_moves['L']
+        if move == 1:
+            if not (self.snake.dir == player_moves['L']).all():
+                self.snake.dir = player_moves['R']
+        if move == 2:
+            if not (self.snake.dir == player_moves['D']).all():
+                self.snake.dir = player_moves['U']
+        if move == 3:
+            if not (self.snake.dir == player_moves['U']).all():
+                self.snake.dir = player_moves['D']
+        self.snake.move()
+        self.time_since_apple += 1
+        # --
+        if self.time_since_apple == 100:  # episode为100时候结束游戏
+            self.game_over = True
+            reward = self.reward_dead
+            self.time_since_apple = 0
+            Done = True
+        # --
+        if self.snake.checkdead(self.snake.pos) == True:  # 碰到边缘和身子，结束游戏
+            self.game_over = True
+            reward = self.reward_dead
+            self.time_since_apple = 0
+            Done = True
+        elif (self.snake.pos == self.apple.pos).all():  # 判断是否吃到苹果
+            self.apple.eaten()
+            self.snake.len += 1
+            self.time_since_apple = 0
+            reward = self.reward_apple
+        len_of_snake = len(self.snake)
+        return reward, Done, len_of_snake
---
+++ run.py
@@ -0,0 +1,158 @@
+import time
+from collections import deque
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from Game import GameEnvironment
+from model import QNetwork, get_network_input
+from replay_buffer import ReplayMemory
+
+epsilon = 0.1
+gridsize = 15
+GAMMA = 0.9
+model = QNetwork(input_dim=12, hidden_dim=256, output_dim=5)
+epStart = 0
+if epStart > 0:
+    model.load_state_dict(torch.load(f'model/model_{epStart}.ln'))
+
+board = GameEnvironment(gridsize, nothing=0, dead=-1, apple=1)
+memory = ReplayMemory(1000)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+
+def run_episode(num_games):
+    run = True
+    move = 0
+    games_played = 0
+    total_reward = 0
+    episode_games = 0
+    len_array = []
+
+    while run:
+        state = get_network_input(board)
+        action_0 = model(state)
+        rand = np.random.uniform(0, 1)
+        if rand > epsilon:
+            action = torch.argmax(action_0)
+        else:
+            action = np.random.randint(0, 5)
+
+        ## update_boardstate the same snake till
+        reward, done, len_of_snake = board.update_boardstate(action)
+        next_state = get_network_input(board)
+
+        memory.push(state, action, reward, next_state, done)
+
+        total_reward += reward
+
+        episode_games += 1
+
+        if board.game_over == True:
+            games_played += 1
+            len_array.append(len_of_snake)
+            board.resetgame()
+
+            if num_games == games_played:
+                run = False
+
+    avg_len_of_snake = np.mean(len_array)
+    max_len_of_snake = np.max(len_array)
+    return total_reward, avg_len_of_snake, max_len_of_snake
+
+
+MSE = nn.MSELoss()
+
+
+def learn(num_updates, batch_size):
+    total_loss = 0
+
+    for i in range(num_updates):
+        optimizer.zero_grad()
+        sample = memory.sample(batch_size)
+
+        states, actions, rewards, next_states, dones = sample
+        states = torch.cat([x.unsqueeze(0) for x in states], dim=0)
+        actions = torch.LongTensor(actions)
+        rewards = torch.FloatTensor(rewards)
+        next_states = torch.cat([x.unsqueeze(0) for x in next_states])
+        dones = torch.FloatTensor(dones)
+
+        q_local = model.forward(states)
+        next_q_value = model.forward(next_states)
+
+        Q_expected = q_local.gather(1, actions.unsqueeze(0).transpose(0, 1)).transpose(0, 1).squeeze(0)
+
+        Q_targets_next = torch.max(next_q_value, 1)[0] * (torch.ones(dones.size()) - dones)
+
+        Q_targets = rewards + GAMMA * Q_targets_next
+
+        loss = MSE(Q_expected, Q_targets)
+
+        total_loss += loss
+        loss.backward()
+        optimizer.step()
+
+    return total_loss
+
+
+# num_episodes = 60000
+num_episodes = 10000
+num_updates = 500
+print_every = 10
+games_in_episode = 30
+batch_size = 20
+
+
+def train():
+    scores_deque = deque(maxlen=100)
+    scores_array = []
+    avg_scores_array = []
+
+    avg_len_array = []
+    avg_max_len_array = []
+
+    time_start = time.time()
+
+    for i_episode in range(epStart, num_episodes + 1):
+
+        ## print('i_episode: ', i_episode)
+
+        score, avg_len, max_len = run_episode(games_in_episode)
+        scores_deque.append(score)
+        scores_array.append(score)
+        avg_len_array.append(avg_len)
+        avg_max_len_array.append(max_len)
+
+        avg_score = np.mean(scores_deque)
+        avg_scores_array.append(avg_score)
+
+        total_loss = learn(num_updates, batch_size)
+
+        dt = (int)(time.time() - time_start)
+
+        if i_episode % print_every == 0 and i_episode > 0:
+            print(
+                'Ep.: {:6}, Loss: {:.3f}, Avg.Score: {:.2f}, Avg.LenOfSnake: {:.2f}, Max.LenOfSnake:  {:.2f} Time: {:02}:{:02}:{:02} '. \
+                    format(i_episode, total_loss, score, avg_len, max_len, dt // 3600, dt % 3600 // 60, dt % 60))
+            # plt.plot(np.arange(1 + epStart, len(avg_len_array) + 1 + epStart), avg_len_array, label="Avg Len of Snake")
+            # plt.plot(np.arange(1 + epStart, len(avg_max_len_array) + 1 + epStart), avg_max_len_array,
+            #          label="Max Len of Snake")
+            # plt.legend(bbox_to_anchor=(1.05, 1))
+            # plt.ylabel('Length of Snake')
+            # plt.xlabel('Episodes #')
+            # plt.show()
+
+        memory.truncate()
+
+        if i_episode % 250 == 0 and i_episode > 0:
+            #     torch.save(model.state_dict(), './dir_chk_len/Snake_{}'.format(i_episode))
+            torch.save(model.state_dict(), f"model/model_{i_episode}.ln")
+
+    return scores_array, avg_scores_array, avg_len_array, avg_max_len_array
+
+
+if __name__ == "__main__":
+    scores, avg_scores, avg_len_of_snake, max_len_of_snake = train()
+    print('length of scores: ', len(scores), ', len of avg_scores: ', len(avg_scores))
